0da82dbf5e9318ba05ff721b5ca47b3dbed085a3
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2021-01-04 13:36:20 +0900

    add contents

---
+++ components/Word.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+interface Props {
+  title?: string;
+  text?: string;
+}
+
+const Word: React.FunctionComponent<Props> = ({ title = "", text = "" }) => (
+  <article>
+    <h2>{title}</h2>
+    {text.split("\n").map((line, index) => (
+      <p key={index}>{line}</p>
+    ))}
+  </article>
+);
+
+export default Word;

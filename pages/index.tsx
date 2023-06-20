d5b896b709180b62bfd7d359cd612b0183c0213c
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2023-06-21 06:29:08 +0900

    use typewriter-effect

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,18 +4,24 @@ import Word from "../components/Word";
 
 import Laboratory from "../contents/laboratory.json";
 
-import Typed from "react-typed";
+import Typewriter from "typewriter-effect";
 
 const IndexPage: React.FunctionComponent = () => {
   return (
     <Layout title="Large-Scale Networking Laboratory">
       <section className="page">
         <h1 className="intro">
-          <Typed
-            strings={[
-              "<span>Network</span> <span>Architecture</span> <span>Laboratory</span>",
-            ]}
-            typeSpeed={50}
+          <Typewriter
+            onInit={(typewriter): void => {
+              typewriter
+                .typeString(
+                  "<span>Network</span> <span>Architecture</span> <span>Laboratory</span>"
+                )
+                .start();
+            }}
+            options={{
+              delay: 75,
+            }}
           />
         </h1>
       </section>
